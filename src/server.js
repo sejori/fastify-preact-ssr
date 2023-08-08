f5e6aaa14cc57cd70e6e823a68788099e55ec26d
Abdessamad El Hamdany <abdessamadelhamdany@hotmail.com>
2023-08-08 19:23:51 +0100

    bump up dependencies versions

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { h } from  'preact'
+import { h } from 'preact'
 import render from 'preact-render-to-string'
 import path from 'path'
 
@@ -6,7 +6,7 @@ import pages from './preact/pages/Index'
 import htmlShell from './templates/default'
 
 const fastify = require('fastify')
-const fastifyStatic = require('fastify-static')
+const fastifyStatic = require('@fastify/static')
 
 // FASTIFY CONFIG
 const app = fastify({ logger: true })
@@ -15,29 +15,31 @@ app.register(fastifyStatic, {
 })
 
 // ROUTES
-pages.forEach(page => app.route({
-    method: 'GET',
-    url: page.route,
-    schema: {
-        response: {
-            200: {
-                type: 'string',
+pages.forEach(page =>
+    app.route({
+        method: 'GET',
+        url: page.route,
+        schema: {
+            response: {
+                200: {
+                    type: 'string'
+                }
             }
+        },
+        handler: function (request, response) {
+            response.header('Content-Type', 'text/html; charset=utf-8')
+            return htmlShell(render(<page.component />))
         }
-    },
-    handler: function (request, response) {
-        response.header('Content-Type', 'text/html; charset=utf-8')
-        return htmlShell(render(<page.component />))
-    }
-}))
+    })
+)
 
 // START SERVER
 const start = async () => {
     try {
-        await app.listen(8888);
+        await app.listen(8888)
     } catch (e) {
-        app.log.error(e);
-        process.exit(1);
+        app.log.error(e)
+        process.exit(1)
     }
 }
 start()
